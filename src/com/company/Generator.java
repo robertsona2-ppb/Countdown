5bace1bfbeae106552fbb90ecf76518a3ce560e0
robertsona2 <Alden.Robertson@ppb.com>
2022-02-23 12:07:39 +0000

    Add the large number generator.

--- src/com/company/Generator.java
+++ src/com/company/Generator.java
@@ -1,26 +1,45 @@
 package com.company;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class Generator {
 
-    public ArrayList<Integer> numbers = new ArrayList<Integer>();
+    public static final int MIN_SMALL_NUM = 1;
+    public static final int MAX = 9;
+    public List<Integer> numbers = new ArrayList<>();
+    public List<Integer> largeNumbers = new ArrayList<>();
+    private Random random = new Random();
+
+    {
+        largeNumbers.add(25);
+        largeNumbers.add(50);
+        largeNumbers.add(75);
+        largeNumbers.add(100);
+    }
 
     private int smallNumGenerator(){
-        int min = 1;
-        int max = 9;
-
-        Random random = new Random();
-
-        int value = random.nextInt(max + min) + min;
-        return value;
+        return random.nextInt(MAX + MIN_SMALL_NUM);
     }
+
+    private int largeNumGenerator(){
+        int index = random.nextInt(largeNumbers.size());
+        int result = largeNumbers.get(index);
+        largeNumbers.remove(index);
+        return result;
+    }
+
 
     public void smallNumSelections(int selections){
         for (int i = 0; i < selections; i++) {
-            System.out.println("hello");
             numbers.add(smallNumGenerator());
+        }
+    }
+    
+    public void largeNumSelections(int selections){
+        for (int i = 0; i < (6 - selections); i++) {
+            numbers.add(largeNumGenerator());
         }
     }
 
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,18 +6,19 @@ public class Main {
 
     static Generator generator = new Generator();
 
-    public static void callGenerator(int selections){
-        generator.smallNumSelections(selections);
-    }
-
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println("Enter the number of small numbers you want!");
 
-        Integer selections = Integer.valueOf(in.nextLine());
+        Integer numOfSmallPicks = Integer.valueOf(in.nextLine());
 
-        callGenerator(selections);
+        callGenerator(numOfSmallPicks);
         System.out.println(generator.numbers);
 
+    }
+
+    public static void callGenerator(int selections){
+        generator.smallNumSelections(selections);
+        generator.largeNumSelections(selections);
     }
 }
